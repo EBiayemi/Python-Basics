db828e2910d78929a756b5fa6cdf3d1d0068f8d9
EBiayemi <ebiayemi4@gmail.com>
2026-07-10 12:43:42 -0400

    Add files via upload

---
+++ Lesson5.py
@@ -0,0 +1,16 @@
+# 1. Store an integer vaule in 'a'
+a = 10 
+# 2. Print the datatype of 'a'
+print(type(a))
+# 3. Store a decimal (float) vaule in 'b' 
+b = 10.5
+# 4. Print the datatype of 'b' 
+print(type(b))
+# 5. Store a text (string) value in 'c'
+c = "Hello"
+# 6. Print the datatype of 'c'
+print(type(c))
+# 7. Store a boolean value in (True/False) in 'd'
+d = True
+# 8. Print the datatype of 'd' 
+print(type(d))
