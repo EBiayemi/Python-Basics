7be21e8ff0f863315c4863060e0bbbbdf72fb2cb
EBiayemi <ebiayemi4@gmail.com>
2026-07-10 12:36:27 -0400

    Add files via upload

---
+++ Lesson4.py
@@ -0,0 +1,27 @@
+# 1) Create variables to store different types of vaules 
+name = "John" 
+age = 16
+is_student = True
+weight = 65.5 
+# 2) Print each variable's vaule 
+print("Name:", name)
+print("Age:", age) 
+print("Is student:", is_student)
+print("Weight:", weight)
+# 3) Print the datatype of each variable
+print(type(name))
+print(type(age))
+print(type(is_student))
+print(type(weight))
+# 4) Show a message that type casting will happan next 
+print("Type casting will happen next")
+# 5) Convert age from an integer to a string 
+age = str(age)
+# 6) Print age and its datatype 
+print("Age:", age)
+print(type(age)) 
+# 7) Convert weight from a float to an interger
+weight = int(weight)
+# 8) Print weight and its datatype 
+print ("Weight:", weight)
+print (type(weight))
